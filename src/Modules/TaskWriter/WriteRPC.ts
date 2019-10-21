835d03d2aeb4271dce3a248280994aad035e8a11
Jim Bulkowski <jim.b@paperelectron.com>
2019-10-21 11:47:33 -0400

    Adds automatic RPC handling to tasks called with .rpc

--- src/Modules/TaskWriter/WriteRPC.ts
+++ src/Modules/TaskWriter/WriteRPC.ts
@@ -7,9 +7,12 @@
 
 import {RMQWriter} from "./RMQWriter";
 import Bluebird from 'bluebird'
+import {has} from 'lodash/fp'
 import {defer} from './defer'
 import {v4} from "uuid";
 
+const isSuccess = has('rpc_auto_success')
+const isFailure = has('rpc_auto_failure')
 
 export class WriteRPC extends RMQWriter {
   queueAsserted: boolean
@@ -18,7 +21,7 @@ export class WriteRPC extends RMQWriter {
   replyQueue: any
   callbacks: Map<string, any>
 
-  constructor(config, channel){
+  constructor(config, channel) {
     super(config, channel)
     this.queueAsserted = false
     this.RPCTimeout = config.RPC.defaultTimeout
@@ -27,7 +30,7 @@ export class WriteRPC extends RMQWriter {
     this.callbacks = new Map()
   }
 
-  rpc(msg, options: {timeout?: number} = {}): any {
+  rpc(msg, options: { timeout?: number } = {}): any {
     let localOptions = {
       expiration: options.timeout ? options.timeout + (options.timeout / 2) : this.expireRemote,
       correlationId: v4(),
@@ -43,18 +46,19 @@ export class WriteRPC extends RMQWriter {
         setTimeout(() => {
           this.callbacks.delete(localOptions.correlationId)
           deferred.reject(new Error(`RPC with correlationId "${localOptions.correlationId} timed out.`))
-        },options.timeout || this.RPCTimeout)
+        }, options.timeout || this.RPCTimeout)
 
         return deferred.promise
       })
   }
-  reply(msg, options){
+
+  reply(msg, options) {
     let replyTo = options.replyTo
     delete options.replyTo
     return this.writeToQueue(replyTo, msg, options)
   }
 
-  initialize(){
+  initialize() {
     return Bluebird.props({
       replyQueue: this.channel.assertQueue('', {exclusive: true, autoDelete: true}),
       taskQueue: this.channel.assertQueue(this.queue, this.queueOptions)
@@ -63,18 +67,30 @@ export class WriteRPC extends RMQWriter {
         this.queueAsserted = true
         this.replyQueue = qs.replyQueue.queue
 
-        this.channel.consume(this.replyQueue, (msg)=>{
+        this.channel.consume(this.replyQueue, (msg) => {
           let correlationId = msg.properties.correlationId
-          if(this.callbacks.has(correlationId)){
+          if (this.callbacks.has(correlationId)) {
             let p = this.callbacks.get(correlationId)
             this.callbacks.delete(correlationId)
 
             try {
-              return p.resolve(JSON.parse(msg.content.toString()))
-            }
-            catch(e){
+              let messageContents = JSON.parse(msg.content.toString())
+              if (isSuccess(messageContents)) {
+                return p.resolve(messageContents.rpc_auto_success)
+              }
+              if (isFailure(messageContents)) {
+                let wf = messageContents.rpc_auto_failure
+                let e = new Error(wf.message || 'No Message in RPC Error.')
+                let stack = wf.stack ? wf.stack : e.stack
+                e.stack = stack
+                return p.reject(e)
+              }
+
+              return p.resolve(messageContents)
+            } catch (e) {
               return p.reject(e)
             }
+
           }
         }, {noAck: true})
 
